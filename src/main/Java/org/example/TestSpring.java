6941d0cb256188c296d0fd8118864e3793be7a50
Pavlentiy <vskyhlam@gmail.com>
2023-04-02 23:42:35 +0700

    DependencyInjection - внедрение зависимости xml

--- src/main/Java/org/example/TestSpring.java
+++ src/main/Java/org/example/TestSpring.java
@@ -7,11 +7,11 @@ public class TestSpring {
     public static void main (String [] args){
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
-        Music music = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(music);
+        // Music music = context.getBean("musicBean", Music.class);
+        // MusicPlayer musicPlayer = new MusicPlayer(music);
+
+        MusicPlayer musicPlayer = context.getBean("MusicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
-
-
 
         context.close();
     }
