1e3099f89be988c5c2598a916fd18fa5afe4ecfb
Pavlentiy <vskyhlam@gmail.com>
2023-04-02 22:45:02 +0700

    Initial commit

---
+++ src/main/Java/org/example/TestBean.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class TestBean {
+
+    private String Name;
+
+    public TestBean(String name) {this.Name = name;}
+
+    public String getName() {return Name;}
+
+    public void setName(String name) {this.Name = name;}
+}
